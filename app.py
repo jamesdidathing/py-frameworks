670bcfedb9cc2c23e5e38c2db42eef094626506a
xq0472 <james.hodson@ukaea.uk>
2023-05-19 08:31:15 +0100

    added table

--- app.py
+++ app.py
@@ -9,6 +9,19 @@ df = pd.DataFrame({
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
+df_tab = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
 
 fig = px.bar(df, x='Fruit', y='Amount', color='City', barmode="group")
 fig.update_layout(
@@ -21,8 +34,6 @@ fig.update_layout(
         yaxis_gridcolor="#e9e9e9",
 )
 
-
-
 app.layout = html.Div(style={"backgroundColor": "#d2d1d1"},
     children=[
     html.H1(className='main-header', children = "My Dash App"),
@@ -30,7 +41,8 @@ app.layout = html.Div(style={"backgroundColor": "#d2d1d1"},
         className="graph1",
         id='example-graph',
         figure=fig
-    )
+    ),
+    generate_table(df_tab)
     ]
 
 )
