d89fb934b01cd1efae925ab064672097e055a721
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-24 15:32:54 +0100

    added POST so that the server can retrieve browser input and validate it

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app_package import app
 from app_package.forms import LoginForm
 
@@ -18,9 +18,17 @@ def index():
             'body': 'The soup recipe needs more salt..'
         }
     ]
+    # render_template will render the html to the page
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+
+# 'POST' method is sending data/whatever from the browser to the server, 'GET' is the 
+# other way around.
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Login', form=form)
