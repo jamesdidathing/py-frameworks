f02c0d59c640fadcced6c6e1516dd3e94f5972e0
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-24 09:12:57 +0100

    used index.html from templates to create our title

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,10 +1,8 @@
+from flask import render_template
 from app_package import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello"
-
-@app.route('/welcome')
-def welcome():
-    return "Welcome!"
+    user = {'username': 'James'}    # this is a mock user
+    return render_template('index.html', title='Home', user=user)
