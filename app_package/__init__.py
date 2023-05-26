a7e86a26e55b9de64059f3e54a7fb5fe31e9be1b
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-26 21:19:42 +0100

    Initialised the database with sqlalchemy and the migration engine to migrate changes in the db

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -3,11 +3,17 @@ Flask to read the config we made for the secret key. """
 
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # This tells flask what the starting point is 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Initialise the database and migration engine
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # Routes are the different URL's that the application implements. It is not at the top 
-# because it is called from app
-from app_package import routes  # noqa: E402, F401
+# because it is called from app. Models is where we designed our database
+from app_package import routes, models  # noqa: E402, F401
