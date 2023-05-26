811eb0e64a87240cc7d27b46a97235c193616a5b
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-26 21:21:42 +0100

    added env variable for the new database

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
-""" Flask and some extensions use the value of the secret key as a cryptographic key to 
-generate signatures or tokens. The first terms (SECRET_KEY) looks for an environment 
-variable of the same name. The idea is that a value sourced from an environment variable
-is preferred, but if the environment does not define the variable, then the hardcoded 
-string is used instead as a default. For this case, security is low. """
+""" This is the place where Flask itself puts certain configuration values and also
+where extensions can put their configuration values. """
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Best practice is to set configuration from environmental variables and then have
+# fallback value.
 class Config(object):
+
+    # Cryptographic key for signatures and tokens. Atm, it is simple
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # This would send a signal to the application everytime the database changes
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
