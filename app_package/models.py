859896ecd78d8b821245e0b9f0bc94998a4ca6d1
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-28 14:50:38 +0100

    created posts table and linked Users with posts

--- app_package/models.py
+++ app_package/models.py
@@ -1,14 +1,20 @@
 """ Creating our database here (schema). The primary key is the ID, and the email and 
 usernames are unique values. The passwords are not directly stored in the databse, but 
-instead the hashes are which is a security best practice. """
+instead the hashes are which is a security best practice. db.relationship initliases the
+posts field in Users. This is a high level view.
+
+The posts database has a foreign key which links the user_id to the id found in the 
+users database. This links the two."""
 
 from app_package import db 
+from datetime import datetime
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """Useful for debugging, will return the username.
@@ -18,3 +24,11 @@ class User(db.Model):
         """
         return '<User {}>'.format(self.username)
     
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
