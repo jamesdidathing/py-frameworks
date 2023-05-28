a3c020652f83020557b382cb364d5f1c494a6186
jamesdidathing <jamesalexhodson@gmail.com>
2023-05-28 15:19:21 +0100

    added flask shell import database atuomatically

--- flaskapp.py
+++ flaskapp.py
@@ -1,2 +1,6 @@
-from app_package import app
+from app_package import app, db
+from app_package.models import User, Post
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
