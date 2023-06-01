8cba1d9ef4f3c373252b15fd984c5ba04b610b49
jamesdidathing <jamesalexhodson@gmail.com>
2023-06-01 10:40:21 +0100

    added UserMixin to class

--- app_package/models.py
+++ app_package/models.py
@@ -9,8 +9,9 @@ users database. This links the two."""
 from app_package import db 
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
