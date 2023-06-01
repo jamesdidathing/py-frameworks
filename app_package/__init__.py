3d97cd93ec71cb216a7cf23f237cb7d065968903
jamesdidathing <jamesalexhodson@gmail.com>
2023-06-01 10:40:46 +0100

    initliazed login manager

--- app_package/__init__.py
+++ app_package/__init__.py
@@ -5,14 +5,17 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # This tells flask what the starting point is 
 app = Flask(__name__)
 app.config.from_object(Config)
+login = LoginManager(app)
 
 # Initialise the database and migration engine
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 # Routes are the different URL's that the application implements. It is not at the top 
 # because it is called from app. Models is where we designed our database
