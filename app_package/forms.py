cf28674f543081ab7aa24ca835a968f9175b525a
jamesdidathing <jamesalexhodson@gmail.com>
2023-06-07 11:03:31 +0100

    created registration class, checks for duplicate emails and usernames

--- app_package/forms.py
+++ app_package/forms.py
@@ -3,7 +3,8 @@ It creates objects for HTML forms such as text fields, buttons, menus. """
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app_package.models import User
 
 class LoginForm(FlaskForm):
 
@@ -13,3 +14,21 @@ class LoginForm(FlaskForm):
 
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
