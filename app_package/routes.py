35269759c7e2a2db47b6815deb19e0f9c5e534d0
jamesdidathing <jamesalexhodson@gmail.com>
2023-06-29 08:55:37 +0100

    adjusted user route for follow/unfollow form

--- app_package/routes.py
+++ app_package/routes.py
@@ -82,7 +82,8 @@ def user(username):
         {'author': user, 'body': 'Test post 1'},
         {'author': user, 'body': 'Test post 2'}
     ]
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 @app.route('/edit_profile', methods=["GET", "POST"])
 @login_required
