465d6b4968414e92b8fc48149a284c565942d851
jamesdidathing <jamesalexhodson@gmail.com>
2023-07-07 11:11:12 +0100

    added prev and next pages to user page now

--- app_package/routes.py
+++ app_package/routes.py
@@ -83,12 +83,16 @@ def logout():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404() # Returns 404 if no user found
-    posts = [
-        {'author': user, 'body': 'Test post 1'},
-        {'author': user, 'body': 'Test post 2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items, 
+                           next_url=next_url, prev_url=prev_url, form=form)
 
 @app.route('/edit_profile', methods=["GET", "POST"])
 @login_required
