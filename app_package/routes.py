e19fdabb7cbc313308c7d213282986f337e16cd3
jamesdidathing <jamesalexhodson@gmail.com>
2023-07-07 10:28:06 +0100

    added prev and next url to be used for the page links

--- app_package/routes.py
+++ app_package/routes.py
@@ -29,8 +29,14 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
     # render_template will render the html to the page
-    return render_template('index.html', title='Home Page', form=form, posts=posts.items)
+    return render_template('index.html', title='Home Page', form=form, 
+                           posts=posts.items, next_url=next_url,
+                           prev_url=prev_url)
 
 
 # 'POST' method is sending data/whatever from the browser to the server, 'GET' is the 
@@ -143,4 +149,9 @@ def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    return render_template("index.html", title='Explore', posts=posts.items,
+                          next_url=next_url, prev_url=prev_url)
