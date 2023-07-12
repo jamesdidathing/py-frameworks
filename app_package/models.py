c283c8c0e94fb44d874ce994ccc277a43f96d06e
jamesdidathing <jamesalexhodson@gmail.com>
2023-07-12 15:17:48 +0100

    added password reset token method and verification static method

--- app_package/models.py
+++ app_package/models.py
@@ -10,9 +10,11 @@ The application needs to help flask-login with the database using the load user
 function."""
 
 from app_package import db 
-from app_package import login
+from app_package import login, app
 
 from datetime import datetime
+import jwt
+from time import time
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
@@ -82,6 +84,21 @@ class User(UserMixin, db.Model):
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256')
+        
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+                            algorithms = ['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
